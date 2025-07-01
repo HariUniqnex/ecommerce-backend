f7db8455baf166cd7656535a66c9bc9a6d9ec8b9
Harisankar <harisankar@uniqnex360.com>
2025-07-01 17:35:18 +0530

    localhost 3000 added for testing

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:3000"
     "https://ecommerce-frontend-slug.onrender.com",
     "http://127.0.0.1:5173",
 ]
